6565d7feacc95c78cff7039f0dab2b39048d46fa
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-01 16:23:19 -0700

    Add feature test for Parks Index

---
+++ spec/features/parks/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "parks index page", type: :feature do
+  it 'can see all park names' do
+    park_1 = Park.create!(name: "Rocky Mountain National Park",
+                          entrance_fee: 50,
+                          ocean_access: true)
+    visit "/parks"
+
+    expect(page).to have_content(park_1.name)
+  end
+end 
