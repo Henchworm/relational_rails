5cfc9f0881deda0ace374837ebca731382828727
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-03 14:54:55 -0700

    Create model #count instance method and test

--- spec/features/parks/index_spec.rb
+++ spec/features/parks/index_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe "parks index page", type: :feature do
     expect(page).to have_content(park_1.name)
     expect(page).to have_content(park_2.name)
   end
+
+  it 'can see all park names' do
+    park_1 = Park.create!(name: "Rocky Mountain National Park",
+                          entrance_fee: 50,
+                          ocean_access: true)
+    park_2 = Park.create!(name: "Glacier National Park",
+                          entrance_fee: 30,
+                          ocean_access: false)
+
+    visit "/parks"
+
+    expect(page).to have_content(park_1.created_at)
+    expect(page).to have_content(park_2.created_at)
+  end
 end
