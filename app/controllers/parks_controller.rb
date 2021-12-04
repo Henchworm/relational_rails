b1f22d2c5939319d20f66b8be9741c9a5832522d
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-04 12:54:34 -0700

    Add park creation tests and methods

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -6,4 +6,17 @@ class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    park = Park.new({
+      name: params[:name],
+      entrance_fee: params[:entrance_fee],
+      ocean_access: params[:ocean_access]
+      })
+    park.save
+    redirect_to "/parks"
+  end
 end
