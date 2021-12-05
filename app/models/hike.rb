8461c9b5b3ec63a058264ac622d03083daf1c86c
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-05 14:03:24 -0700

    Add ::true_only to hike model

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -5,4 +5,8 @@ class Hike < ApplicationRecord
   validates_presence_of :mileage
   validates :water_on_route, inclusion: [true, false]
   validates_presence_of :elevation_gain
+
+  def self.true_only
+    Hike.where(water_on_route: "true")
+  end
 end
