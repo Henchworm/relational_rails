8ffd4c9cfd320e13beb64ef32e98841cc053282e
Chris Hewitt <agop5134@gmail.com>
2021-12-06 17:54:13 -0700

    Create User Story 22 and spec

--- spec/features/mtn_ranges/index_spec.rb
+++ spec/features/mtn_ranges/index_spec.rb
@@ -39,4 +39,34 @@ RSpec.describe "mtnrange index page", type: :feature do
     click_link('New Mountain Range')
     expect(current_path).to eq("/mtn_ranges/new")
   end
+
+  it 'show link to delete mtn_range' do
+    mtn_range_1 = MtnRange.create!(name: "Sierra Nevada",
+                          natl_park_land: true,
+                          drivetime: 16)
+
+    mtn_range_2 = MtnRange.create!(name: "Rocky Mountains",
+                          natl_park_land: false,
+                          drivetime: 16)
+    visit "/mtn_ranges"
+
+    expect(page).to have_link("Delete Sierra Nevada")
+    expect(page).to have_link("Delete Rocky Mountains")
+  end
+
+  it 'can link to delete a mountain range next to each mountain range' do
+    mtn_range_1 = MtnRange.create!(name: "Sierra Nevada",
+                          natl_park_land: true,
+                          drivetime: 16)
+
+    mtn_range_2 = MtnRange.create!(name: "Rocky Mountains",
+                          natl_park_land: false,
+                          drivetime: 16)
+    visit "/mtn_ranges"
+    click_link("Delete Sierra Nevada")
+    expect(current_path).to eq("/mtn_ranges")
+    expect(page).to_not have_content("#{mtn_range_1.name}")
+    expect(page).to have_content("#{mtn_range_2.name}")
+  end
+
 end
